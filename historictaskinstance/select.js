c8676d7a23517418da35908b9050b2d38dab5b74
fatfish <fatfish2010@sina.cn>
2012-06-29 10:10:14 +0800

    增加流程监控、任务监控视图点击打开工作空间方法

---
+++ historictaskinstance/select.js
@@ -0,0 +1,27 @@
+bc.historicTaskInstanceSelectView = {
+	/** 点击确认按钮后的处理函数 */
+	clickOk : function() {
+var $page = $(this);
+		
+		// 获取选中的行的id单元格
+		var $tds = $page.find(".bc-grid>.data>.left tr.ui-state-highlight>td.id");
+		if($tds.length == 0){
+			alert("请先选择！");
+			return false;
+		}
+
+		// 获取选中的数据
+		var data;
+		var $grid = $page.find(".bc-grid");
+		var $tr = $grid.find(">.data>.right tr.ui-state-highlight");
+		var $hidden=$tr.data("hidden");
+		
+		var procinstid=$hidden.procinstid;
+		var url=bc.root+"/bc-workflow/workspace/open?id=";
+		$.ajax({
+			url:url+procinstid,
+			success:function(data){
+			}
+		});
+	}
+};
